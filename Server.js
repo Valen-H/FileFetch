6a9805a17ccfa9811d479437c32804ed49e60e63
ValentinHacker <alternativexxxy@gmail.com>
2018-02-24 16:13:58 +0200

    Update (#3)
    Patches.

--- Server.js
+++ Server.js
@@ -1,12 +1,14 @@
 const http = require("http"), url = require("url"), fs = require("fs-extra"), stream = require("stream");
-const PING_INTERVAL = 5000;
-module.exports = function server({ port = process.env.port || process.env.npm_package_config_port || 8080, pass = "herokujspass", silence = false, ping = PING_INTERVAL } = { }) {
+const PING_INTERVAL = 4000;
+module.exports = function server({ port = process.env.port || process.env.PORT || process.env.npm_package_config_port || 8080, pass = "herokujspass", silence = false, ping = process.env.ping || PING_INTERVAL } = { }) {
 	var srv, console = global.console;
 	//localize console
 	if (silence) console = new console.constructor(new stream.Writable(), new stream.Writable());
 	//redirect local console to null stream, silencing
 	(srv = http.createServer((req, res) => {
 		var q = url.parse(req.url, true);
+		req.on("error", console.error);
+		res.on("error", console.error);
 		if (q.query.pass == pass) {
 			res.writeHead(200, http["200"]);
 			if (q.query.command) {
@@ -21,26 +23,31 @@ module.exports = function server({ port = process.env.port || process.env.npm_pa
 				res.reload = dt => res.write(dt || "reload");
 				res.load = dt => res.write("load");
 				srv.connect.push(res);
-				res.emit("connected");
+				res.emit("connected", res);
 				srv.emit("connected", res);
 				return true;
 			} else if (q.pathname == "/close") {
 				res.end("<h1>CLOSED</h1>");
-				srv.emit("closed");
-				res.emit("closed");
+				srv.emit("closed", srv);
+				res.emit("closed", srv);
 				console.info("Server Closed.");
+				srv.connect.concat(srv.events).forEach(conn => {
+					conn.setKeepAlive(false);
+					conn.end("\r\nCLOSED. \r\n");
+					clearInterval(srv.ping);
+				});
 				srv.close();
 				return true;
 			} else if (q.pathname == "/ping") {
 				res.write(process.uptime() + "");
-				srv.emit("pinged");
-				res.emit("pinged");
+				srv.emit("pinged", process.uptime());
+				res.emit("pinged", process.uptime());
 			} else if (q.pathname == "/store") {
 				fs.ensureFileSync(q.query.path);
 				req.pipe(fs.createWriteStream(q.query.path || "store.txt"));
-				res.write("loaded");
-				res.emit("loaded");
-				srv.emit("loaded");
+				res.write("loaded", q.query.path);
+				res.emit("loaded", q.query.path);
+				srv.emit("loaded", q.query.path);
 				console.info(`${q.query.path} Loaded.`);
 			} else if (q.pathname == "/event") {
 				/*if (q.query.data) {
@@ -76,12 +83,16 @@ module.exports = function server({ port = process.env.port || process.env.npm_pa
 		res.end("", "utf-8");
 	})).listen(port, () => {
 		console.info(`Server bound to port ${port}`);
-	});
+	}).on("error", console.error);
 	srv.connect = [];
 	srv.events = [];
+	srv.on("close", () => {
+		console.info("Server Closed.");
+		srv.emit("closed", srv);
+	});
 	if (ping) {
-		setInterval(() => {
-			http.get({ host: "127.0.0.1", port: port, path: "/ping" }, ignore => { });
+		srv.ping = setInterval(() => {
+			http.get({ host: process.env.app || "127.0.0.1", port: port, path: "/ping?pass=" + pass }, ignore => { }).once("error", () => clearInterval(srv.ping));
 		}, ping);
 	}
 	return srv;
